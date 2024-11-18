0a8952e6af5adaac8c646e9f832dad7056b4d675
angelinaezhou <aezhou721@gmail.com>
2024-11-18 17:32:49 -0600

    more menu button

--- Project/find-my-illini/src/App.jsx
+++ Project/find-my-illini/src/App.jsx
@@ -29,6 +29,8 @@ const MenuButton = () => {
     }
   };
 
+  {isOpen && <div className="backdrop" onClick={toggleMenu}></div>}
+
   useEffect(() => {
     document.addEventListener('mousedown', handleClickOutside);
     return () => {
